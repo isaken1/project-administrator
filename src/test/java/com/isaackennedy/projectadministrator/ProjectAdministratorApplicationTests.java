a60e3917d0da4c1196e2d09bf7c8ffaf14f9042f
Isaac Kennedy <isaac_kennedy@hotmail.com>
2021-06-27 16:58:47 -0300

    Correções no arquivo de configurações

--- src/test/java/com/isaackennedy/projectadministrator/ProjectAdministratorApplicationTests.java
+++ src/test/java/com/isaackennedy/projectadministrator/ProjectAdministratorApplicationTests.java
@@ -3,10 +3,10 @@ package com.isaackennedy.projectadministrator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProjectAdministratorApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
